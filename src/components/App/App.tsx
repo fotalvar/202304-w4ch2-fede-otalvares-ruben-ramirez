6363153cf3c9cf88fe5086c5a38515d67f1f1630
RubenRaMar <rubenfr1@hotmail.com>
2023-04-25 23:11:50 +0200

    Add content to the App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,21 @@
 const App = (): JSX.Element => {
-  return <p></p>;
+  return (
+    <div className="container">
+      <header className="main-header">
+        <img
+          className="main-logo"
+          src="images/main-logo.webp"
+          alt="stars words logo"
+          width="200"
+          height="96"
+        />
+        <h1 className="main-title">Characters Stars Words</h1>
+      </header>
+      <section className="main">
+        <ul className="characters"></ul>
+      </section>
+    </div>
+  );
 };
 
 export default App;
