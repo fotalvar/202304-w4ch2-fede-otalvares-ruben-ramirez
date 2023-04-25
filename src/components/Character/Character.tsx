a9c17b073d11ba6150801abac70a99a6f6d43d81
Fede <fnotalvares@gmail.com>
2023-04-25 22:59:19 +0200

    Add character component and CharacterStructure interface

---
+++ src/components/Character/Character.tsx
@@ -0,0 +1,23 @@
+import { CharacterStructure } from "../../types";
+
+interface CharacterProps {
+  character: CharacterStructure;
+}
+
+const Character = ({
+  character: { name, picture, heigth, mass, created },
+}: CharacterProps): JSX.Element => {
+  return (
+    <li className="character">
+      <div className="character__container">
+        <h2 className="character__name">{name}</h2>
+        <img className="character__image" src={`img/${picture}`} alt={name} />
+        <span className="character__details">Heigth: {heigth}</span>
+        <span className="character__details">Mass: {mass}</span>
+        <span className="character__details">Creation date: {created}</span>
+      </div>
+    </li>
+  );
+};
+
+export default Character;
---
+++ src/types.tsx
@@ -0,0 +1,8 @@
+export interface CharacterStructure {
+  id: number;
+  name: string;
+  picture: string;
+  heigth: number;
+  mass: number;
+  created: string;
+}
